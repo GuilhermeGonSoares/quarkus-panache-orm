071fc09a135ae3553148ccef4b24e2bf1a9fa4fc
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2024-07-18 10:50:39 -0300

    create publisher test

--- vintage-store/src/test/java/quarkus/course/panache/PublisherTest.java
+++ vintage-store/src/test/java/quarkus/course/panache/PublisherTest.java
@@ -1,4 +1,21 @@
+package quarkus.course.panache;
+
+import io.quarkus.test.TestTransaction;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+@QuarkusTest
 class PublisherTest {
-  
+
+    @Test
+    @TestTransaction
+    public void shouldCreateAndFindAPublisher() {
+        Publisher publisher = new Publisher("Packt");
+        publisher.persist();
+
+        Publisher foundPublisher = Publisher.findById(publisher.id);
+        assertNotNull(foundPublisher);
+        assertEquals("Packt", foundPublisher.name);
+    }
 }
