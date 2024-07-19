28a4387072a70b743c831699eb2f484636751ed6
Guilherme Gonçalves <guilherme16.gon@gmail.com>
2024-07-18 23:04:20 -0300

    create publisher controller

--- vintage-store/src/main/java/quarkus/course/panache/controllers/PublisherController.java
+++ vintage-store/src/main/java/quarkus/course/panache/controllers/PublisherController.java
@@ -4,25 +4,25 @@ import jakarta.inject.Inject;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import quarkus.course.jdbc.Artist;
+import quarkus.course.panache.model.Publisher;
 import quarkus.course.panache.repository.ArtistRepository;
 
 import java.util.List;
 
-@Path("/api/artists")
+@Path("/api/publishers")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
-public class ArtistController {
-    @Inject
-    ArtistRepository artistRepository;
+public class PublisherController {
+
 
     @GET
     @Path("{id}")
-    public Artist findArtistById(@PathParam("id") Long id) {
-        return artistRepository.findByIdOptional(id).orElseThrow(NotFoundException::new);
+    public Publisher findPublisherById(@PathParam("id") Long id) {
+        return (Publisher) Publisher.findByIdOptional(id).orElseThrow(NotFoundException::new);
     }
 
     @GET
-    public List<Artist> findAllArtists() {
-        return artistRepository.listAllArtistsSorted();
+    public List<Publisher> findAllArtists() {
+        return Publisher.listAll();
     }
 }
